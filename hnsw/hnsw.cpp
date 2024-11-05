595b5b89e2bd2456817cea8c909a9d32de349d11
PolarisSoda <urin187@gmail.com>
2024-11-05 10:13:11 +0900

    Test

--- hnsw/hnsw.cpp
+++ hnsw/hnsw.cpp
@@ -89,11 +89,11 @@ void HNSWGraph::Insert(Item& q) {
 
 	// search up layer entrance
 	int ep = enterNode;
-	for (int i = maxLyer; i > l; i--) ep = searchLayer(q, ep, 1, i)[0];
-	
+	for (int i = maxLyer; i > l; i--) ep = searchLayer(items[nid], ep, 1, i)[0];
+
     for (int i = min(l, maxLyer); i >= 0; i--) {
         int MM = l == 0 ? MMax0 : MMax;
-        vector<int> neighbors = searchLayer(q, ep, efConstruction, i); // neighbor를 efConstruction만큼 찾는다.
+        vector<int> neighbors = searchLayer(items[nid], ep, efConstruction, i); // neighbor를 efConstruction만큼 찾는다.
         vector<int> selectedNeighbors = vector<int>(neighbors.begin(), neighbors.begin() + min(int(neighbors.size()), M)); // 최대 M개 까지의 이웃을 선택한다.
 
         #pragma omp critical
