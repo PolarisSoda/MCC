832dcea2b1b1469db5e7e30a84055f56ba67ef1e
PolarisSoda <urin187@gmail.com>
2024-11-05 15:13:11 +0900

    Concurrency Check

--- hnsw/hnsw.cpp
+++ hnsw/hnsw.cpp
@@ -95,7 +95,7 @@ void HNSWGraph::Insert(Item& q) {
 
 	// search up layer entrance
 	int ep = enterNode;
-	#pragma omp for firstprivate(q,ep) lastprivate(ep)
+	#pragma omp for firstprivate(q)
 	for (int i = maxLyer; i > l; i--) {
 		cout << omp_get_thread_num() << " " << &q << endl;
 		ep = searchLayer(q, ep, 1, i)[0];
