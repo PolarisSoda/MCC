26fb12fcd114f351c35664167fef8542a79e86c4
PolarisSoda <urin187@gmail.com>
2024-11-08 08:00:28 +0900

    Test for Concurrency

--- hnsw/hnsw.cpp
+++ hnsw/hnsw.cpp
@@ -9,19 +9,127 @@
 #include <vector>
 #include <omp.h>
 #include <atomic>
+#include <cstdlib>
 
 using namespace std;
 
+void HNSWGraph::SearchTemp(
+	int thread_id,
+	vector<set<pair<double,int>>>& local_candidates,
+	vector<set<pair<double,int>>>& local_nearestNeighbors,
+	unordered_set<int>& isVisited,
+	omp_lock_t &lock_isVisited,
+	int lc,
+	int ef, Item& q) 
+	{
+	while (!local_candidates[thread_id].empty()) {
+		auto ci = local_candidates[thread_id].begin(); local_candidates[thread_id].erase(local_candidates[thread_id].begin());
+		int nid = ci->second;
+		auto fi = local_nearestNeighbors[thread_id].end(); fi--;
+
+		if (ci->first > fi->first) break;
+
+		for (int ed: layerEdgeLists[lc][nid]) {
+			omp_set_lock(&lock_isVisited);
+			if (isVisited.find(ed) != isVisited.end()) {
+				omp_unset_lock(&lock_isVisited);
+				continue;
+			} else {
+				isVisited.insert(ed);
+				omp_unset_lock(&lock_isVisited);
+			}
+			
+			fi = local_nearestNeighbors[thread_id].end(); fi--;
+			double td = q.dist(items[ed]);
+
+			
+			if ((td < fi->first) || local_nearestNeighbors[thread_id].size() < ef) {
+				int param = rand()%2;
+				if(param) {
+					local_candidates[thread_id].insert(make_pair(td, ed));
+					local_nearestNeighbors[thread_id].insert(make_pair(td, ed));
+					if (local_nearestNeighbors[thread_id].size() > ef) local_nearestNeighbors[thread_id].erase(fi);
+				} else {
+					#pragma omp task firstprivate(td,ed,lc)
+					{
+						int new_thread_id = omp_get_thread_num();
+						local_candidates[thread_id].insert(make_pair(td,ed));
+						local_nearestNeighbors[thread_id].insert(make_pair(td,ed));
+						SearchTemp(new_thread_id,local_candidates,local_nearestNeighbors,isVisited,lock_isVisited,lc,ef,q);
+					}
+				}
+				
+			}
+		}
+	}
+}
+
 vector<int> HNSWGraph::searchLayer(Item& q, int ep, int ef, int lc) {
 	set<pair<double, int>> candidates;
-	set<pair<double, int>> nearestNeighbors;
-	unordered_set<int> isVisited;
+	set<pair<double, int>> nearestNeighbors; //These values will be global.
 
 	double td = q.dist(items[ep]);
-	candidates.insert(make_pair(td, ep));
-	nearestNeighbors.insert(make_pair(td, ep));
-	isVisited.insert(ep);
 
+	unordered_set<int> isVisited;
+	isVisited.insert(ep);
+	omp_lock_t lock_isVisited;
+	omp_init_lock(&lock_isVisited);
+
+	int thread_cnt = omp_get_num_threads();
+	int local_ef = ef / thread_cnt + 10;
+
+	vector<set<pair<double,int>>> local_candidates(thread_cnt);
+	vector<set<pair<double,int>>> local_nearestNeightbors(thread_cnt);
+
+	local_candidates[0].insert(make_pair(td,ep));
+	local_nearestNeightbors[0].insert(make_pair(td,ep));
+	
+	#pragma omp parallel num_threads(thread_cnt)
+	{
+		#pragma omp master
+		{	
+			while(!local_candidates[0].empty()) {
+				auto ci = local_candidates[0].begin();
+				local_candidates[0].erase(local_candidates[0].begin());
+				int nid = ci->second;
+
+				auto fi = local_nearestNeightbors[0].end(); fi--;
+
+				if(ci->first > fi->first) break;
+
+				for(int ed : layerEdgeLists[lc][nid]) {
+					omp_set_lock(&lock_isVisited);
+					if (isVisited.find(ed) != isVisited.end()) {
+						omp_unset_lock(&lock_isVisited);
+						continue;
+					} else {
+						isVisited.insert(ed);
+						omp_unset_lock(&lock_isVisited);
+					}
+
+					fi = local_nearestNeightbors[0].end(); fi--;
+					double new_td = q.dist(items[ed]);
+
+					bool first = true;
+					if ((td < fi->first) || local_nearestNeightbors[0].size() < local_ef) {
+						if(first) {
+							local_candidates[0].insert(make_pair(new_td,ed));
+							local_nearestNeightbors[0].insert(make_pair(new_td,ed));
+							if (nearestNeighbors.size() > local_ef) nearestNeighbors.erase(fi);
+						} else {
+							#pragma omp task firstprivate(new_td,ed,lc)
+							{
+								int thread_id = omp_get_thread_num();
+								local_candidates[thread_id].insert(make_pair(new_td,ed));
+								local_nearestNeightbors[thread_id].insert(make_pair(new_td,ed));
+								SearchTemp(thread_id,local_candidates,local_nearestNeightbors,isVisited,lock_isVisited,lc,local_ef,q);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 
 	while (!candidates.empty()) {
 		auto ci = candidates.begin(); candidates.erase(candidates.begin());
--- hnsw/hnsw.h
+++ hnsw/hnsw.h
@@ -73,7 +73,14 @@ struct HNSWGraph {
 	vector<int> searchLayer(Item& q, int ep, int ef, int lc);
 	void Insert(Item& q);
 	vector<int> KNNSearch(Item& q, int K);
-
+	void HNSWGraph::SearchTemp(
+	int thread_id,
+	vector<set<pair<double,int>>>& local_candidates,
+	vector<set<pair<double,int>>>& nearestNeighbors,
+	unordered_set<int>& isVisited,
+	omp_lock_t &lock_isVisited,
+	int lc,
+	int ef, Item& q) ;
 
 	// This will not be used....
 	void printGraph() {
