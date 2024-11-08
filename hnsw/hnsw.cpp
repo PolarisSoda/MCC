3a4bb459650d5d805b77621ba4e0215f58488738
PolarisSoda <urin187@gmail.com>
2024-11-08 13:39:03 +0900

    Not Good

--- hnsw/hnsw.cpp
+++ hnsw/hnsw.cpp
@@ -93,7 +93,7 @@ vector<int> HNSWGraph::searchLayer(Item& q, int ep, int ef, int lc) {
 		alignas(64) vector<set<pair<double,int>>> local_nearest(40);
 		alignas(64) vector<unordered_set<int>> local_visit(40);
 
-		#pragma omp parallel num_threads(40) shared(local_cand,local_nearest,local_visit)
+		#pragma omp parallel num_threads(40)
 		{
 			#pragma omp for schedule(dynamic)
 			for(int j=0; j<layersize; j++) {
