b27b332427138c58f46cc2fe3aaee0864e4d9474
PolarisSoda <urin187@gmail.com>
2024-11-25 14:26:48 +0900

    Update references

--- stringsort/ref.cu
+++ stringsort/ref.cu
@@ -1,77 +1,49 @@
 #include <cuda_runtime.h>
 #include <stdio.h>
 
-#define MAX_LEN 30
-#define NUM_BUCKETS 256
-
-__global__ void countSortKernel(char *d_input, char *d_output, int *d_count, int exp, int n, int str_len) {
+__global__ void doubleArrayKernel(int *d_array, int rows, int cols) {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
-    if (i < n) {
-        for (int j = 0; j < str_len; j++) {
-            atomicAdd(&d_count[(d_input[i * str_len + j] / exp) % NUM_BUCKETS], 1);
-        }
-    }
-    __syncthreads();
-
-    if (i < NUM_BUCKETS) {
-        for (int j = 1; j < NUM_BUCKETS; j++) {
-            d_count[j] += d_count[j - 1];
-        }
-    }
-    __syncthreads();
-
-    if (i < n) {
-        for (int j = str_len - 1; j >= 0; j--) {
-            int pos = atomicSub(&d_count[(d_input[i * str_len + j] / exp) % NUM_BUCKETS], 1) - 1;
-            d_output[pos * str_len + j] = d_input[i * str_len + j];
-        }
+    int j = blockIdx.y * blockDim.y + threadIdx.y;
+    int index = i * cols + j;
+    if (i < rows && j < cols) {
+        d_array[index] *= 2;
     }
 }
 
-void radixSort(char h_input[][MAX_LEN], int n) {
-    char *d_input, *d_output;
-    int *d_count;
-    size_t size = n * MAX_LEN * sizeof(char);
-    size_t count_size = NUM_BUCKETS * sizeof(int);
+void doubleArray(int *h_array, int rows, int cols) {
+    int *d_array;
+    size_t size = rows * cols * sizeof(int);
 
     // 메모리 할당
-    cudaMalloc((void**)&d_input, size);
-    cudaMalloc((void**)&d_output, size);
-    cudaMalloc((void**)&d_count, count_size);
+    cudaMalloc((void**)&d_array, size);
 
     // 호스트에서 디바이스로 데이터 복사
-    cudaMemcpy(d_input, h_input, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_array, h_array, size, cudaMemcpyHostToDevice);
 
     // CUDA 커널 호출
-    int blockSize = 256;
-    int numBlocks = (n + blockSize - 1) / blockSize;
-
-    for (int exp = 1; exp < NUM_BUCKETS; exp *= NUM_BUCKETS) {
-        cudaMemset(d_count, 0, count_size);
-        countSortKernel<<<numBlocks, blockSize>>>(d_input, d_output, d_count, exp, n, MAX_LEN);
-        cudaDeviceSynchronize();
-
-        // 결과를 d_input으로 복사
-        cudaMemcpy(d_input, d_output, size, cudaMemcpyDeviceToDevice);
-    }
+    dim3 blockSize(16, 16);
+    dim3 numBlocks((rows + blockSize.x - 1) / blockSize.x, (cols + blockSize.y - 1) / blockSize.y);
+    doubleArrayKernel<<<numBlocks, blockSize>>>(d_array, rows, cols);
+    cudaDeviceSynchronize();
 
     // 결과를 호스트로 복사
-    cudaMemcpy(h_input, d_output, size, cudaMemcpyDeviceToHost);
+    cudaMemcpy(h_array, d_array, size, cudaMemcpyDeviceToHost);
 
     // 메모리 해제
-    cudaFree(d_input);
-    cudaFree(d_output);
-    cudaFree(d_count);
+    cudaFree(d_array);
 }
 
 int main() {
-    char h_input[][MAX_LEN] = {"hi", "hello", "you", "this", "is", "for", "ttttt"};
-    int n = sizeof(h_input) / MAX_LEN;
+    int h_array[3][2] = {{1, 2}, {3, 4}, {5, 6}};
+    int rows = 3;
+    int cols = 2;
 
-    radixSort(h_input, n);
+    doubleArray((int*)h_array, rows, cols);
 
-    for (int i = 0; i < n; i++) {
-        printf("Sorted string: %s\n", h_input[i]);
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("Array element [%d][%d]: %d\n", i, j, h_array[i][j]);
+        }
     }
 
     return 0;
