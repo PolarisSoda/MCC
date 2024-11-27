b4ecde21535ed0adea21668ec3ca9003024d0fd6
PolarisSoda <urin187@gmail.com>
2024-11-27 16:44:09 +0900

    Improve single block algorithm

--- stringsort/main_final.cu
+++ stringsort/main_final.cu
@@ -15,8 +15,8 @@ __global__ void kernel_function(char* device_input, char* device_output, char** 
     __shared__ int histogram[CHAR_RANGE]; //global historam
     __shared__ int offset[CHAR_RANGE]; //global offset
     __shared__ int count[CHAR_RANGE]; //global count
-    //declare local variable
 
+    //declare local variable
     int idx = threadIdx.x; // thread's index
     int workload = (N + NUM_THREADS - 1) / NUM_THREADS; //각 스레드가 가지는 문자열의 양.
     int start_pos = threadIdx.x * workload; // 0: 0~195 1: 196~391 //각 스레드가 시작할 위치.
@@ -26,11 +26,9 @@ __global__ void kernel_function(char* device_input, char* device_output, char** 
     for(int i=start_pos; i<end_pos; i++) input_index[i] = device_input + i*MAX_LEN;
     __syncthreads();
 
-    // prefix_offset[idx][i]는 idx번째 스레드까지 i문자의 합.
     for(int pos=MAX_LEN-1; pos>=0; pos--) {
         // INIT global variable
         if(idx < CHAR_RANGE) histogram[idx] = 0, count[idx] = 0;
-        for(int i=0; i<CHAR_RANGE; i++) prefix_offset[idx][i] = 0;
         __syncthreads();
 
         int local_histogram[CHAR_RANGE] = {0,};
@@ -38,11 +36,7 @@ __global__ void kernel_function(char* device_input, char* device_output, char** 
             char now = input_index[i][pos];
             local_histogram[now-64]++;
         }
-
-        for(int i=0; i<CHAR_RANGE; i++) {
-            atomicAdd(&histogram[i],local_histogram[i]);
-            prefix_offset[idx][i] += local_histogram[i];
-        }
+        for(int i=0; i<CHAR_RANGE; i++) atomicAdd(&histogram[i],local_histogram[i]);
         __syncthreads();
 
         if(idx == 0) {
@@ -50,7 +44,7 @@ __global__ void kernel_function(char* device_input, char* device_output, char** 
             for(int i=0; i<CHAR_RANGE-1; i++) offset[i+1] = offset[i] + histogram[i];
         }
         __syncthreads();
-
+        
         for(int i=0; i<N; i++) {
             char now = input_index[i][pos];
             int index = now - 64;
