2c484800a573642462afb789a989e3ab3e274c29
PolarisSoda <urin187@gmail.com>
2024-11-29 19:17:26 +0900

    Test

--- stringsort/main_multi_block.cu
+++ stringsort/main_multi_block.cu
@@ -102,12 +102,13 @@ __global__ void kernel_merge(char* device_input, char* device_output, char** inp
     for(int i=0; i<1; i++) {
         if(i%2 == 0) {
             if(idx % 2 == 0) {
-                int write_cur = start_pos[idx];
                 int left_cur = start_pos[idx];
                 int left_end = end_pos[idx];
                 int right_cur = start_pos[idx+1];
                 int right_end = end_pos[idx+1];
+                int write_cur = start_pos[idx];
 
+                printf("%d %d %d %d\n",left_cur,left_end,right_cur,right_end);
                 while(left_cur < left_end && right_cur < right_end) {
                     char* left_str = left_cur == left_end ? MAX_INF_STR : input_index[left_cur];
                     char* right_str = right_cur == right_end ? MAX_INF_STR : input_index[right_cur];
