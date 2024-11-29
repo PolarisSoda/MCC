2a29032e142d455568c7d16149b1d2c13d271020
PolarisSoda <urin187@gmail.com>
2024-11-29 14:20:24 +0900

    Added multi block version

--- stringsort/main_multi_block.cu
+++ stringsort/main_multi_block.cu
@@ -68,7 +68,7 @@ __global__ void kernel_function(char* device_input, char* device_output, char** 
             char now = input_index[i][pos];
             int index = now - 64;
 
-            int after_index = block_start_pos + block_offset[index] + prefix_count[index] + local_count[index]++ -1;
+            int after_index = block_start_pos + block_offset[index] + prefix_count[index] + local_count[index]++;
             assert(after_index >= block_start_pos && after_index < block_end_pos);
             output_index[after_index] = input_index[i];
         }
