3e9495d2544fa262cf96f4d48c33e9bb91c5b4d4
PolarisSoda <urin187@gmail.com>
2024-11-29 12:24:51 +0900

    Added multi block version

--- stringsort/main_multi_block.cu
+++ stringsort/main_multi_block.cu
@@ -67,9 +67,11 @@ __global__ void kernel_function(char* device_input, char* device_output, char** 
             char now = input_index[i][pos];
             int index = now - 64;
 
-            int after_index = block_offset[index] + prefix_count[index] + local_count[index]++;
+            int after_index = block_start_pos + block_offset[index] + prefix_count[index] + local_count[index]++;
             output_index[after_index] = input_index[i];
         }
+
+        for(int i=thread_start_pos; i<thread_end_pos; i++) input_index[i] = output_index[i];
         __syncthreads();
     }
 
