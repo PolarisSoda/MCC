d22605c15b0d10d00cb9ce35634a505573253bbd
PolarisSoda <urin187@gmail.com>
2024-11-29 14:26:16 +0900

    Added multi block version

--- stringsort/main_multi_block.cu
+++ stringsort/main_multi_block.cu
@@ -67,8 +67,8 @@ __global__ void kernel_function(char* device_input, char* device_output, char** 
         for(int i=thread_start_pos; i<thread_end_pos; i++) {
             char now = input_index[i][pos];
             int index = now - 64;
-            assert(index >= 0 && index < 60);
-            int after_index = block_start_pos + block_offset[index] + prefix_count[index] + local_count[index]++;
+
+            int after_index = blockIdx.x == 0 ? 0 : block_start_pos-1 + block_offset[index] + prefix_count[index] + local_count[index]++;
             assert(after_index >= block_start_pos && after_index < block_end_pos);
             output_index[after_index] = input_index[i];
         }
