c543f918c4acb820d6fda8c6a5272a92ec16c7b7
PolarisSoda <urin187@gmail.com>
2024-12-08 10:57:05 +0900

    W

--- rwho/who_svc.c
+++ rwho/who_svc.c
@@ -20,7 +20,7 @@
 char** get_logged_users_1_svc(void *a, struct svc_req *b) {
     struct utmp *entry;
     static const size_t MAX_BUFFER_SIZE = 100000; // Fixed buffer size
-    char *result = NULL;
+    static char *result = NULL;
 
     if(result != NULL) {
         free(result);
